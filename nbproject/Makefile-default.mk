29dbff99b8382370c4cde7e8921a52e8263e140c
Oleksii Pysarenko <pysarole@gmail.com>
2023-08-06 16:27:40 +0200

    Reduce the contents of Makefiles

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -1,6 +1,3 @@
-# This makefile implements configuration specific macros and targets.
-
-
 # Include project Makefile
 ifeq "${IGNORE_LOCAL}" "TRUE"
 # do not include local makefile. User is passing all local related variables already
@@ -44,26 +41,19 @@ else
 SUB_IMAGE_ADDRESS_COMMAND=
 endif
 
-# Object Directory
 OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 
-# Distribution Directory
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
-# Source Files Quoted if spaced
 SOURCEFILES_QUOTED_IF_SPACED=main.asm
 
-# Object Files Quoted if spaced
 OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.obj
 POSSIBLE_DEPFILES=${OBJECTDIR}/main.obj.d
 
-# Object Files
 OBJECTFILES=${OBJECTDIR}/main.obj
 
-# Source Files
 SOURCEFILES=main.asm
 
-# Pack Options 
 PACK_ASSEMBLER_OPTIONS=-I "${DFP_DIR}/avrasm/inc"  -i m328Pdef.inc
 
 
@@ -72,13 +62,6 @@ CFLAGS=
 ASFLAGS=
 LDLIBSOPTIONS=
 
-############# Tool locations ##########################################
-# If you copy a project from one host to another, the path where the  #
-# compiler is installed may be different.                             #
-# If you open this project with MPLAB X in the new host, this         #
-# makefile will be regenerated and the paths will be corrected.       #
-#######################################################################
-# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
 FIXDEPS=fixDeps
 
 .build-conf:  ${BUILD_SUBPROJECTS}
@@ -87,8 +70,7 @@ ifneq ($(INFORMATION_MESSAGE), )
 endif
 	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
-# ------------------------------------------------------------------------------------
-# Rules for buildStep: assemble
+
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${OBJECTDIR}/main.obj: main.asm  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} ${DISTDIR} 
@@ -103,8 +85,7 @@ ${OBJECTDIR}/main.obj: main.asm  nbproject/Makefile-${CND_CONF}.mk
 	${MP_AS}  -fI -W+ie ${PACK_ASSEMBLER_OPTIONS} -d ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  -S ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.tmp  -o ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.hex  -m ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.map  -l ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.lss main.asm
 endif
 
-# ------------------------------------------------------------------------------------
-# Rules for buildStep: link
+
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	
@@ -114,11 +95,8 @@ ${DISTDIR}/Lab9.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Make
 endif
 
 
-# Subprojects
 .build-subprojects:
 
-
-# Subprojects
 .clean-subprojects:
 
 # Clean Targets
--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -1,5 +1,4 @@
 CND_BASEDIR=`pwd`
-# default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
 CND_ARTIFACT_NAME_default=Lab9.X.production.obj
 CND_ARTIFACT_PATH_default=dist/default/production/Lab9.X.production.obj
